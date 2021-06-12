c845d7dd87bc673662fe4fb60d4c42631349089f
13263081188 <1057113249@qq.com>
2021-06-13 01:24:35 +0800

    sss

--- demo/app.py
+++ demo/app.py
@@ -41,7 +41,7 @@ def main():
         print("test_in_out")
         st.set_page_config(layout="wide",page_title="变分自动编码器可视化解释器")
         # Designing the interface
-        for kla in os.listdir()[0]:
+        for kla in os.listdir():
             m += kla
         st.title(m)
         # For newline
